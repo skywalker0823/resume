98ed3927817e1be74f6bcc0b28cfc7575c9b97ba
eddie <eddie@lacoal.net>
2023-09-22 13:49:24 +0800

    test react app upload ok, and please wake the fxck up :)#2

--- resume-app/src/components/Counter.js
+++ resume-app/src/components/Counter.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+const url = "https://ww5vcheuc4pqbjchcnw2bbm2ym0ppnmq.lambda-url.ap-northeast-2.on.aws/";
 
-const url = "https://ww5vcheuc4pqbjchcnw2bbm2ym0ppnmq.lambda-url.ap-northeast-2.on.aws/"
+export default function Counter() {
+  const [count, setCount] = useState(null);
 
-export default async function Counter() {
-    let response = await fetch(url);
-    let data = await response.json();
-    let count = data;
-    
-    return (
-        <div>
-            <h1>目前訪客數量:{count}</h1>
-        </div>
-    );
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let response = await fetch(url);
+        let data = await response.json();
+        setCount(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []); // 空依賴數組表示僅在組件初次渲染時執行一次
+
+  return (
+    <div>
+      <h1>目前訪客數量: {count !== null ? count : "Loading..."}</h1>
+    </div>
+  );
 }
