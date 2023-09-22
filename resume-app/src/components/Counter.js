19528f4beca091fd6fd67a08beab85948cf823f0
eddie <eddie@lacoal.net>
2023-09-22 13:20:24 +0800

    teat react app upload

--- resume-app/src/components/Counter.js
+++ resume-app/src/components/Counter.js
@@ -4,11 +4,9 @@ import React from "react";
 const url = "https://ww5vcheuc4pqbjchcnw2bbm2ym0ppnmq.lambda-url.ap-northeast-2.on.aws/"
 
 export default async function Counter() {
-    const [count, setCount] = React.useState(0);
-
-    let response = fetch(url);
+    let response = await fetch(url);
     let data = await response.json();
-    setCount(data);
+    let count = data;
     
     return (
         <div>
